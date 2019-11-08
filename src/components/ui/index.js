fa243beaf95d9dc68b75914ff2701ec4ce0f1bf6
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-11-08 11:55:28 -0500

    typo

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -88,7 +88,7 @@ export const SideNav = styled.div`
     font-size: 18px;
     line-height: 28px;
     color: #dbdbdb;
-    text-alight: right;
+    text-align: right;
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     text-decoration: none;
     -webkit-touch-callout: none;
