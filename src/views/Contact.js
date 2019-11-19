1865fcb961f167940cd2f6355262cb086d9a98be
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-19 10:33:34 -0600

    setting up contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contactPage">
+      <form>
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
--- src/views/Features.js
+++ src/views/Features.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import profilePlaceholder from '../assets/img/person-icon-1675.png'
+import profilePlaceholder from '../assets/img/person-icon-1675.png';
 
 const Features = () => {
   return (
@@ -7,23 +7,36 @@ const Features = () => {
       <div className="outerFeature">
         <img src={profilePlaceholder} alt="profile" width="100" />
         <h3>Lorem Ipsum</h3>
-        <p>Lorem ipsum dolor sit amet, consectepor pharetra. Phasellus gravida, ante eu pretium placerat, nunc libero euismod lectus, nec mollis odio eros vitae lorem. Nulla sed luctus tellus. Nullam t risus.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectepor pharetra. Phasellus gravida,
+          ante eu pretium placerat, nunc libero euismod lectus, nec mollis odio
+          eros vitae lorem. Nulla sed luctus tellus. Nullam t risus.
+        </p>
         <button>BUTTON</button>
       </div>
       <div className="middleFeature">
         <img src={profilePlaceholder} alt="profile" width="100" />
         <h3>Lorem Ipsum</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum est non nisi tempor pharetra. Phasellus gravida, ante eu pretium placerat, nunc libero euismod lectus, nec mex. Aliquam ante velit, sagittis nec urna eget, ultrices volutpat risus.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum
+          est non nisi tempor pharetra. Phasellus gravida, ante eu pretium
+          placerat, nunc libero euismod lectus, nec mex. Aliquam ante velit,
+          sagittis nec urna eget, ultrices volutpat risus.
+        </p>
         <button>BUTTON</button>
       </div>
       <div className="outerFeature">
         <img src={profilePlaceholder} alt="profile" width="100" />
         <h3>Lorem Ipsum</h3>
-        <p>Lorem ipsum dolor sit ams vitae lorem. Nulla sed luctus tellus. Nullam et consectetur ex. Aliquam ante velit, sagittis nec urna eget, ultrices volutpat risus.</p>
+        <p>
+          Lorem ipsum dolor sit ams vitae lorem. Nulla sed luctus tellus. Nullam
+          et consectetur ex. Aliquam ante velit, sagittis nec urna eget,
+          ultrices volutpat risus.
+        </p>
         <button>BUTTON</button>
       </div>
     </div>
-  )
+  );
 };
 
 export default Features;
