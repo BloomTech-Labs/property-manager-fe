74c1974f113c65f3a2bae9ac8c1fffe5bdebbba3
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-19 14:08:07 -0600

    setting up contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -5,13 +5,16 @@ const Contact = () => {
     <div className="contactPage">
       <form>
         <h1>Contact</h1>
-        <input type="text" placeholder="Email" name="email"/>
-        <textarea name="message" placeholder="Write your message here..." cols="50" rows="10"></textarea>
+        <input type="text" placeholder="Email" name="email" />
+        <textarea
+          name="message"
+          placeholder="Write your message here..."
+          cols="50"
+          rows="10"
+        />
         <button>SEND</button>
       </form>
-      <div className="contactInfo">
-        placeholder
-      </div>
+      <div className="contactInfo">placeholder</div>
     </div>
   );
 };
