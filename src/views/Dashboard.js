5778557f8874c35c984e0c3a7e702685df87279f
Tyler Turnipseed <tyler@techturnip.us>
2019-11-23 14:58:32 -0500

    hooked add property form up to back end

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+// UI Components
+import { useDispatch } from 'react-redux';
 import { VertNav } from '../components/navigation';
+
+// Add Property Form
 import AddPropertyForm from '../components/AddPropertyForm/AddPropertyForm';
 
+// Redux imports
+import { createProperty } from '../store/actions';
+
+const dispatchAddProperty = createProperty(
+  'https://pt6-propman.herokuapp.com/api/properties'
+);
+
 const Dashboard = () => {
-  const handleLog = values => console.log(values);
+  const dispatch = useDispatch();
+
+  const handleAddProperty = useCallback(
+    ({ ...values }) => dispatch(dispatchAddProperty(values)),
+    [dispatch]
+  );
 
   return (
     <div className="dashboard">
       <VertNav />
       <h2>Dashboard</h2>
-      <AddPropertyForm submit={handleLog} />
+      <AddPropertyForm submit={handleAddProperty} />
     </div>
   );
 };
