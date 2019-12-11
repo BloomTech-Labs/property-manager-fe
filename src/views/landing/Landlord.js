77df08a4c1644989a2054b64901516d4c9d53c6a
anthonyvigz <anthonyvigz@gmail.com>
2019-12-10 21:04:07 -0500

    set up landlord and renter main jsx

--- src/views/landing/Landlord.js
+++ src/views/landing/Landlord.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import calendarImage from '../../assets/img/undraw_calendar_dutt.png';
 
 const Landlord = () => {
-  return <div className="landlordView">Landlord</div>;
+  return (
+    <div className="landlordView">
+      <div className="landlordBlock">
+        <div className="landlordHeader">
+          <div className="headerBlock">
+            <h3>Lorem Ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectepor pharetra. Phasellus
+              gravida, ante eu pretium placerat, nunc libero euismod lectus, nec
+              mollis odio eros vitae lorem. Nulla sed luctus tellus. Nullam t
+              risus.
+            </p>
+          </div>
+          <img src={calendarImage} alt="calendar" width="50px" />
+        </div>
+        <div className="landlordSubs">
+          <div className="subBlock">
+            <h3>Lorem Ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectepor pharetra. Phasellus
+              gravida, ante eu pretium placerat, nunc libero euismod lectus, nec
+              mollis odio eros vitae lorem. Nulla sed luctus tellus. Nullam t
+              risus.
+            </p>
+          </div>
+          <div className="subBlock">
+            <h3>Lorem Ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectepor pharetra. Phasellus
+              gravida, ante eu pretium placerat, nunc libero euismod lectus, nec
+              mollis odio eros vitae lorem. Nulla sed luctus tellus. Nullam t
+              risus.
+            </p>
+          </div>
+          <div className="subBlock">
+            <h3>Lorem Ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectepor pharetra. Phasellus
+              gravida, ante eu pretium placerat, nunc libero euismod lectus, nec
+              mollis odio eros vitae lorem. Nulla sed luctus tellus. Nullam t
+              risus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landlord;
