df0676b22ffbe871aa7ca2bd37f347d80bf0ee8c
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-12-14 21:36:40 -0500

    So stuck on materialui, not a huge fan

--- src/views/dashboard/profile/Profile.js
+++ src/views/dashboard/profile/Profile.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import { Skeleton } from '@material-ui/lab';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProperties } from '../../../store/actions';
 import {
   Divider,
   Grid,
   Paper,
   CardHeader,
+  CardContent,
   Typography
 } from '@material-ui/core';
-import PropertyList from '../../../components/Properties/PropertyList';
+import ProfileCard from './ProfileCard';
+import PropertyCard from '../../../components/Properties/PropertyCard';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const propertyList = useSelector(state => state.propReducer.properties);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(
+        getProperties(
+          'https://pt6-propman-staging.herokuapp.com/api/propertieshttps://pt6-propman-staging.herokuapp.com/api/properties'
+        )
+      ).then(() => {
+        setLoading(false);
+      });
+    }, 2000);
+  }, [dispatch]);
+
   return (
     <div className="profile">
       <h1>User Profile</h1>
       <Divider />
       <br />
-      <Grid container spacing={3}>
-        <Grid key="userId" item xs={12} sm={6} md={4} lg={3}>
-          <Paper elevation={5}>
-            <CardHeader
-              title={<Typography variant="body1">User's Name</Typography>}
-            />
-          </Paper>
-        </Grid>
-        <Grid key="properties" item xs={12} sm={6} md={4} lg={3}>
-          <Paper elevation={5}>
-            <PropertyList />
-          </Paper>
-        </Grid>
-      </Grid>
+      <ProfileCard />
     </div>
   );
 }
