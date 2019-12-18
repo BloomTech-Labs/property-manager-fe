91422b564512db1e9a4abde02035f807f0fd6d54
Teddy <tfikrie@yahoo.com>
2019-12-17 21:58:54 -0500

    Front page adjusted to include short advertising and add the sign up page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Tenant from './views/landing/Tenant';
 import Features from './views/landing/Features';
 import Contact from './views/landing/Contact';
 import FrontPage from './views/landing/FrontPage';
-import GetStarted from './views/landing/GetStarted';
+import GetStarted from './views/SignUp';
 import ProtectedRoutes from './components/Auth/Routes/ProtectedRoutes';
 
 setDefaultBreakpoints([{ mobile: 250 }, { tablet: 769 }, { desktop: 1025 }]);
