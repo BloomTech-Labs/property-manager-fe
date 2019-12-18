5f0b888e591c17dad97b5bebb8cae6d3d07e7061
Tyler Turnipseed <tyler@techturnip.us>
2019-12-18 18:54:27 -0500

    added svg to overview

--- src/views/dashboard/overview/Overview.js
+++ src/views/dashboard/overview/Overview.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import UnderConstructionSVG from '../../../components/SVG/UnderConstructionSVG';
 
 export default function Overview() {
   return (
     <div className="overview">
-      <h2>Overview modules are coming soon...</h2>
+      <h2 style={{ textAlign: 'center' }}>
+        Overview modules are coming soon...
+      </h2>
+      <UnderConstructionSVG />
     </div>
   );
 }
