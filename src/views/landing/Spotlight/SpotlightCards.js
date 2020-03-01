4070902688089521f243b11a0139a3b330f49be0
nick.durbin <nick.durbin@gmail.com>
2020-02-29 22:55:51 -0500

    Component to display all the Spotlight cards.

---
+++ src/views/landing/Spotlight/SpotlightCards.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SpotlightCard from './SpotlightCard';
+
+const SpotlightCards = () => {
+  return (
+    <div className="spotlightCards">
+      <SpotlightCard />
+      <SpotlightCard />
+      <SpotlightCard />
+      <SpotlightCard />
+    </div>
+  );
+};
+
+export default SpotlightCards;
