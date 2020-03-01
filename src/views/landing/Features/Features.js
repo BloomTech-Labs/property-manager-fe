cc9099bda28da8d7e4e94fa958b191cba504998a
nick.durbin <nick.durbin@gmail.com>
2020-02-29 20:18:38 -0500

    Refactor of the features component view.

---
+++ src/views/landing/Features/Features.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FeatureHeading from './FeatureHeading';
+import FeatureTable from './FeatureTable';
+
+const Features = () => {
+  return (
+    <>
+      <FeatureHeading />
+      <FeatureTable />
+    </>
+  );
+};
+
+export default Features;
