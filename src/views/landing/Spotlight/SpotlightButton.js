a3de12bb6b4c0f6e3a0430d0b15411a7f83ee88b
ndurbin <nick.durbin@gmail.com>
2020-03-02 04:54:51 -0500

    Added the AuthFlip functionaity with the custom useModal hook to create the same functionality as the login nav link.

--- src/views/landing/Spotlight/SpotlightButton.js
+++ src/views/landing/Spotlight/SpotlightButton.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useModal } from '../../../hooks/useModal';
+import { AuthFlip } from '../../../components/Auth/AuthFlip';
 
 const SpotlightButton = () => {
+  const { isShowing, close, toggle } = useModal();
+
   return (
     <div className="btnWrapper">
-      <button className="spotBtn" type="submit">
-        Sign Up
+      <button className="spotBtn" type="submit" onClick={() => toggle()}>
+        Sign up
       </button>
+      <AuthFlip close={close} isShowing={isShowing} />
     </div>
   );
 };
