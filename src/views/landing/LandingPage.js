9eeac3cfea50a71c54ef039979d459b70f513714
nick.durbin <nick.durbin@gmail.com>
2020-03-04 21:11:21 -0500

    Removed the Nav component to the App file.

--- src/views/landing/LandingPage.js
+++ src/views/landing/LandingPage.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { HorNav } from '../../components/Navigation/navigation';
 import Spotlight from './Spotlight/Spotlight';
 import Features from './Features/Features';
 import Header from './Header';
@@ -9,10 +8,11 @@ import Header from './Header';
 function LandingPage() {
   return (
     <div className="landing-container">
-      <HorNav />
-      <Header />
-      <Spotlight />
-      <Features />
+      <div className="landing-content">
+        <Header />
+        <Spotlight />
+        <Features />
+      </div>
     </div>
   );
 }
