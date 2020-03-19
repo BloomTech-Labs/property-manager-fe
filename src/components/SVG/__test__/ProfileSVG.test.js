e44902d8ace29094dd3d57b0c3202d9cd54f2220
nick.durbin <nick.durbin@gmail.com>
2020-03-19 19:21:22 -0400

    Deleted the function svg files and the test folder for those svg functions.

--- src/components/SVG/__test__/ProfileSVG.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'enzyme';
-import ProfileSVG from '../ProfileSVG';
-
-it('renders an svg', () => {
-  const component = render(<ProfileSVG />);
-
-  expect(component.is('svg')).toEqual(true);
-});
