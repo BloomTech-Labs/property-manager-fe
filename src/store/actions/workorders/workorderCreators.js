0cebd3a784b529ebc00a4295043209fc01fb6b29
ndurbin <nick.durbin@gmail.com>
2020-04-09 17:54:36 -0400

    Separated the workorderCreators from the index file.

---
+++ src/store/actions/workorders/workorderCreators.js
@@ -0,0 +1,78 @@
+import {
+  ADD_WORK_ORDER_START,
+  ADD_WORK_ORDER_FAIL,
+  ADD_WORK_ORDER_SUCCESS,
+  GET_WORK_ORDERS_START,
+  GET_WORK_ORDERS_FAIL,
+  GET_WORK_ORDERS_SUCCESS,
+  UPDATE_WORK_ORDER_START,
+  UPDATE_WORK_ORDER_FAIL,
+  UPDATE_WORK_ORDER_SUCCESS
+} from './workorderTypes';
+import { baseUrl } from '../../../helpers/baseUrl';
+import axiosAuth from '../../../helpers/axiosAuth';
+import { showSuccessToast, showErrorToast } from '../toastActions';
+
+export const getWorkOrders = () => async dispatch => {
+  dispatch({ type: GET_WORK_ORDERS_START });
+  try {
+    const res = await axiosAuth().get(`${baseUrl}/workorders`);
+    dispatch({
+      type: GET_WORK_ORDERS_SUCCESS,
+      payload: {
+        workOrders: res.data
+      }
+    });
+  } catch (err) {
+    dispatch({ type: GET_WORK_ORDERS_FAIL, payload: { errMsg: err } });
+  }
+};
+// ------------------------------------------------|
+// eslint-disable-next-line no-unused-vars
+export const addWorkOrder = workOrder => async dispatch => {
+  dispatch({ type: ADD_WORK_ORDER_START });
+  try {
+    const res = await axiosAuth().post(`${baseUrl}/workorders`, workOrder);
+
+    // show success toast
+    dispatch(showSuccessToast('Work Order created!'));
+
+    dispatch({
+      type: ADD_WORK_ORDER_SUCCESS,
+      payload: {
+        workOrders: res.data
+      }
+    });
+  } catch (err) {
+    // show error toast
+    dispatch(showErrorToast('Uh oh! Something went wrong'));
+
+    dispatch({ type: ADD_WORK_ORDER_FAIL, payload: { errMsg: err } });
+  }
+};
+// ------------------------------------------------|
+// eslint-disable-next-line no-unused-vars
+export const updateWorkOrder = workOrder => async dispatch => {
+  dispatch({ type: UPDATE_WORK_ORDER_START });
+  try {
+    const res = await axiosAuth().put(
+      `${baseUrl}/workorders/${workOrder.id}`,
+      workOrder
+    );
+
+    // show success toast
+    dispatch(showSuccessToast('Work Order updated!'));
+
+    dispatch({
+      type: UPDATE_WORK_ORDER_SUCCESS,
+      payload: {
+        workOrders: res
+      }
+    });
+  } catch (err) {
+    // show error toast
+    dispatch(showErrorToast('Uh oh! Something went wrong'));
+
+    dispatch({ type: UPDATE_WORK_ORDER_FAIL, payload: { errMsg: err } });
+  }
+};
