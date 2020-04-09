7531ed306c6a8da422500be79ac771388481f82c
ndurbin <nick.durbin@gmail.com>
2020-04-09 17:56:27 -0400

    Separated the userTypes from the index file.

---
+++ src/store/actions/users/userTypes.js
@@ -0,0 +1,6 @@
+export const GET_USER_START = 'GET_USER_START';
+export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
+export const GET_USER_FAIL = 'GET_USER_FAIL';
+export const EDIT_USER_START = 'EDIT_USER_START';
+export const EDIT_USER_SUCCESS = 'EDIT_USER_SUCCESS';
+export const EDIT_USER_FAIL = 'EDIT_USER_FAIL';
