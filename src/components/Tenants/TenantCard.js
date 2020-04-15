768c5668961fde4520f1a13705fddb283dab3919
Cole Gonzales <colegonzales@Coles-MacBook-Pro.local>
2020-04-14 21:32:15 -0500

    Refactored tenants list to use a table instead of cards, decmiated Tenants.js file

--- src/components/Tenants/TenantCard.js
+++ src/components/Tenants/TenantCard.js
@@ -1,118 +1,86 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActionArea,
-  CardActions,
-  Divider
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper
 } from '@material-ui/core';
-import Skeleton from '@material-ui/lab/Skeleton';
+import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
 import { navigate } from '@reach/router';
+import IconButton from '../UI/IconButton';
+import { getTenants } from '../../store/actions';
 
 // Component Styling
 const useStyles = makeStyles({
-  card: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    height: '100%',
-    maxWidth: 345
+  tenantCell: {
+    minWidth: '20vw'
   },
-  upperArea: {
-    height: '100%'
-  },
-  media: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& img': {
-      height: 140
-    }
-  },
-  cardAction: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& svg': {
-      fontSize: '2rem',
-      color: '#76e276',
-      transition: '0.3s'
-    },
-    '&:hover': {
-      '& > svg': {
-        color: '#2d3b4f'
-      }
-    }
+  cell: {
+    minWidth: '15vw'
   }
 });
 
-const TenantCard = ({
-  upperPath,
-  iconPath,
-  title,
-  icon,
-  img,
-  property,
-  handleOpen,
-  tenant
-}) => {
+const TenantCard = () => {
+  const dispatch = useDispatch();
   const classes = useStyles();
 
-  const handleClick = () => {
-    if (handleOpen) {
-      // We call the handleOpen() func passed as prop
-      // and give it the tenant & property that
-      // was passed as props
-      // This gives access to the obj data we can
-      // use in the individual tenant modal
-      return handleOpen(property, tenant);
-    }
+  const tenants = useSelector(state => state.propReducer.tenants);
 
-    return null;
-  };
+  useEffect(() => {
+    dispatch(getTenants());
+  }, [dispatch]);
+
+  console.log('TENANTS', tenants);
+  // const handleClick = () => {
+  //   if (handleOpen) {
+  //     // We call the handleOpen() func passed as prop
+  //     // and give it the tenant & property that
+  //     // was passed as props
+  //     // This gives access to the obj data we can
+  //     // use in the individual tenant modal
+  //     return handleOpen(property, tenant);
+  //   }
+
+  //   return null;
+  // };
 
   return (
-    <Grid className={classes.grid} item xs={12} sm={6} md={4} lg={3}>
-      <Card className={classes.card}>
-        <CardActionArea
-          className={classes.upperArea}
-          onClick={e => {
-            e.stopPropagation();
-            if (upperPath) {
-              navigate(upperPath);
-            }
-
-            handleClick();
-          }}
-        >
-          <CardMedia className={classes.media}>
-            {img || <Skeleton variant="rect" height={140} width="100%" />}
-          </CardMedia>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {title || <Skeleton />}
-            </Typography>
-            {property && <Typography>Resident of {property.name}</Typography>}
-          </CardContent>
-        </CardActionArea>
-
-        <CardActionArea
-          onClick={() => {
-            navigate(iconPath);
-          }}
-        >
-          <Divider />
-          <CardActions className={classes.cardAction}>
-            {icon || <Skeleton variant="circle" width={28} height={28} />}
-          </CardActions>
-        </CardActionArea>
-      </Card>
-    </Grid>
+    <>
+      <IconButton text="Add" icon={<AddIcon />} url="/dashboard/tenants/add" />
+      {tenants.length === 0 ? (
+        <div className={classes.empty}>
+          <h3>No tenants have been added, yet...</h3>
+        </div>
+      ) : (
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tenantCell}>Tenant</TableCell>
+                <TableCell className={classes.cell}>Property</TableCell>
+                <TableCell className={classes.cell}>Unit</TableCell>
+                <TableCell className={classes.cell}>Move In Date</TableCell>
+              </TableRow>
+            </TableHead>
+            {/* Here we separate the table, above are the headings, below are the actual data sects. */}
+            <TableBody>
+              {tenants.map((
+                tenant // Create a row for each tenant
+              ) => (
+                <TableRow>
+                  <TableCell>{tenant.firstName}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      )}
+    </>
   );
 };
 
