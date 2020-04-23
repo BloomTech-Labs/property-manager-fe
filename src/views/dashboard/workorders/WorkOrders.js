0e0e946b10d641c2e519f795783f15e98ee2ca21
nick.durbin <nick.durbin@gmail.com>
2020-04-22 23:18:50 -0400

    Changed back the hard coded array to grab the data via the reducer.

--- src/views/dashboard/workorders/WorkOrders.js
+++ src/views/dashboard/workorders/WorkOrders.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AddIcon from '@material-ui/icons/Add';
 import { Divider } from '@material-ui/core';
 import WorkOrderTable from '../../../components/WorkOrders/WorkOrderTable';
 import IconButton from '../../../components/UI/IconButton';
 
 export default function WorkOrders() {
-  const workOrderList = [];
+  const workOrderList = useSelector(state => state.workOrderReducer.workOrders);
+  console.log(workOrderList, 'In workOrder.js');
 
   return (
     <div className="work-orders">
