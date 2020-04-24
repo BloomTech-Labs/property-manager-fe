7fd32c5f0f2cf4cf1a0bce3b04cbcfeee63edc0f
nick.durbin <nick.durbin@gmail.com>
2020-04-24 00:23:12 -0400

    More small changes.

--- src/components/WorkOrders/NoWorkOrder.js
+++ src/components/WorkOrders/NoWorkOrder.js
@@ -1,17 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Table, TableContainer, Paper } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 
 function NoWorkOrder() {
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table className="table" aria-label="simple table">
-          <h4 className="noWorkOrders">
-            You don&apos;t have any active Work Orders yet!
-          </h4>
-        </Table>
-      </TableContainer>
+      <Paper>
+        <h4 className="noWorkOrders">
+          You don&apos;t have any active Work Orders yet!
+        </h4>
+      </Paper>
     </>
   );
 }
