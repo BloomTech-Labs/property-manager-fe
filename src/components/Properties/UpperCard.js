009cebeb222baf3bcb62f455ac7040cb58d4c06c
ndurbin <nick.durbin@gmail.com>
2020-04-24 06:53:25 -0400

    Extracted the upper part of the propertycard.

---
+++ src/components/Properties/UpperCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { navigate } from '@reach/router';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Skeleton } from '@material-ui/lab';
+
+function UpperCard({ handleClick, props }) {
+  const { title, img, upperPath } = props;
+
+  return (
+    <>
+      <CardActionArea onClick={e => { e.stopPropagation(); navigate(upperPath); handleClick();}}>
+        <CardMedia className="property-card-media">
+          { img || <Skeleton variant="rect" height={140} width="100%" /> }
+        </CardMedia>
+        <CardContent>
+          <h5>{ title || <Skeleton /> }</h5>
+        </CardContent>
+      </CardActionArea>
+    </>
+  );
+}
+
+export default UpperCard;
