b18b4795d9ecbcc11b109871159873d05142a3a7
nick.durbin <nick.durbin@gmail.com>
2020-04-24 08:52:28 -0400

    Added just some simple name text. User information will be undefined because of no DB values.

--- src/views/dashboard/profile/UserCard.js
+++ src/views/dashboard/profile/UserCard.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { CardContent, CardActionArea, CardActions } from '@material-ui/core';
 
-export default function UserCard({
-  user: { firstName, lastName, type },
-  setOpen
-}) {
+export default function UserCard({ user: { firstName, lastName, type }, setOpen }) {
   return (
     <>
       <CardContent>
-        <h2>>User&apos;s Info</h2>
+        <h2>User Info</h2>
       </CardContent>
       <CardActionArea>
-        <CardContent>{`${firstName} ${lastName}`}</CardContent>
+        <CardContent>{`Name:${firstName} ${lastName}`}</CardContent>
         <CardContent>{type}</CardContent>
       </CardActionArea>
       <CardActions>
