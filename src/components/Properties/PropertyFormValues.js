5f919e7b19ac188f96106759e9fb0081f351a502
nick.durbin <nick.durbin@gmail.com>
2020-04-23 23:55:25 -0400

    Working on making the property form smaller for code climate.

---
+++ src/components/Properties/PropertyFormValues.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    className: 'input-wrapper',
+    htmlFor: 'name',
+    html: 'Name',
+    placeholder: 'Enter the name for your Property',
+    name: 'name',
+    type: 'text',
+    touched: 'name',
+    message: 'name'
+  },
+  {
+    className: 'input-wrapper',
+    htmlFor: 'rent',
+    html: 'Rent',
+    placeholder: 'Enter the rent for your Property',
+    name: 'rent',
+    type: 'number',
+    touched: 'rent',
+    message: 'rent'
+  },
+  {
+    className: 'input-wrapper',
+    htmlFor: 'street',
+    html: 'Street',
+    placeholder: 'Street address',
+    name: 'street_address',
+    type: 'text',
+    touched: 'street_address',
+    message: 'street_address'
+  },
+  {
+    className: 'input-wrapper',
+    htmlFor: 'city',
+    html: 'City',
+    placeholder: 'City',
+    name: 'city',
+    type: 'text',
+    touched: 'city',
+    message: 'city'
+  },
+  {
+    className: 'input-wrapper',
+    htmlFor: 'zip',
+    html: 'Zip',
+    placeholder: 'Enter a 5-digit Zip Code',
+    name: 'zip',
+    type: 'text',
+    touched: 'zip',
+    message: 'zip'
+  },
+  {
+    className: 'input-wrapper',
+    htmlFor: 'state',
+    html: 'State',
+    placeholder: 'State',
+    name: 'state',
+    type: 'text',
+    touched: 'state',
+    message: 'state'
+  },
+  {
+    className: 'input-wrapper',
+    htmlFor: 'occupied',
+    html: 'Occupants',
+    placeholder: 'Number of occupants',
+    name: 'occupied',
+    type: 'text',
+    touched: 'occupied',
+    message: 'occupied'
+  }
+];
