03b0c87f7a61e4e078df126b8910f4da0b1f197e
ndurbin <nick.durbin@gmail.com>
2020-04-24 06:54:39 -0400

    Extracted the styles into SASS and extracted the form into a component.

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,21 +1,12 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/prop-types */
 /** @jsx jsx */
-import { Formik, Field, Form } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { jsx } from '@emotion/core';
-import { makeStyles, CircularProgress } from '@material-ui/core';
-import FormErrors from '../../helpers/FormErrors';
 import MuiModal from '../UI/MuiModal';
-
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-    width: '100%',
-    maxWidth: '600px'
-  }
-}));
+import Loading from '../UI/Loading';
+import ProfileFields from './ProfileFields';
 
 const validationSchema = Yup.object().shape({
   firstName: Yup.string()
@@ -31,39 +22,13 @@ export default function ProfileForm({
   submit,
   loading,
   isSubmitting,
-  open,
   opened,
   close
 }) {
-  // eslint-disable-next-line no-unused-vars
-  const classes = useStyles();
-
   const initialValues = { firstName: '', lastName: '', phone: '' };
 
   if (loading || isSubmitting) {
-    return (
-      <div
-        className="form-card"
-        style={{ height: '500px', position: 'relative' }}
-      >
-        <div
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)'
-          }}
-        >
-          <CircularProgress
-            style={{
-              height: '100px',
-              width: '100px'
-            }}
-            color="secondary"
-          />
-        </div>
-      </div>
-    );
+    return <Loading />;
   }
 
   return (
@@ -78,52 +43,7 @@ export default function ProfileForm({
             submit(values);
           }}
         >
-          {({ errors, touched, isSubmitting }) => (
-            <Form data-testid="form-element">
-              <div className="input-wrapper">
-                <label htmlFor="first-name">First Name</label>
-                <Field
-                  placeholder="Update your first name"
-                  name="firstName"
-                  type="text"
-                />
-                <FormErrors
-                  touched={touched.firstName}
-                  message={errors.firstName}
-                />
-              </div>
-              <div className="input-wrapper">
-                <label htmlFor="last-name">Last Name</label>
-                <Field
-                  placeholder="Update your last name"
-                  name="lastName"
-                  type="text"
-                />
-                <FormErrors
-                  touched={touched.lastName}
-                  message={errors.lastName}
-                />
-              </div>
-              <div className="input-wrapper">
-                <label htmlFor="phone">Phone</label>
-                <Field
-                  placeholder="Update your phone number"
-                  name="phone"
-                  type="number"
-                />
-                <FormErrors touched={touched.phone} message={errors.phone} />
-              </div>
-              <div className="submit-btn-wrapper">
-                <button
-                  className="btn btn-animated"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Submit
-                </button>
-              </div>
-            </Form>
-          )}
+          <ProfileFields />
         </Formik>
       </div>
     </MuiModal>
