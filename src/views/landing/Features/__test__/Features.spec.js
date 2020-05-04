1715c5fa00e593e92307bb90bf65ba37c178e1d1
nick.durbin <nick.durbin@gmail.com>
2020-05-04 16:46:04 -0400

    Generic test to see if component is rendering.

---
+++ src/views/landing/Features/__test__/Features.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Features from '../Features';
+
+afterEach(rtl.cleanup);
+
+test('Makes sure the word Freehold exists on LP', async () => {
+  const wrapper = rtl.render(<Features />);
+  expect(await wrapper.queryAllByText(/professional dashboard/i));
+});
