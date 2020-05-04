46d17db8b8a6a6ba059a3806d5cc09e90409b443
nick.durbin <nick.durbin@gmail.com>
2020-05-04 16:47:01 -0400

    testing the modal popup and navigation.

--- src/views/landing/Contact/__test__/ContactForm.spec.js
+++ src/views/landing/Contact/__test__/ContactForm.spec.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import ContactForm from '../ContactForm';
+import renderWithRouter from '../../../../helpers/renderWithRouter';
+import ContactFormMessage from '../ContactFormMessage';
 
 afterEach(rtl.cleanup);
 
-test('Checks submit button event and navigate to home', async () => {});
+test('Checks submit button event for modal', async () => {
+  const { container, getByTestId } = await rtl.render(<ContactForm />);
+
+  const appContainer = container;
+  expect(await appContainer.innerHTML).toMatch('Contact Us');
+
+  const leftClick = { button: 0 };
+  rtl.fireEvent.click(getByTestId(/contactBtn/i), leftClick);
+});
+
+test('shows the modal message after submit', async () => {
+  const formMessage = 'Form Submitted!';
+  const { container, getByText } = await rtl.render(<ContactForm />);
+  const {
+    modal,
+    history: { navigate }
+  } = renderWithRouter(<ContactFormMessage>{formMessage}</ContactFormMessage>);
+
+  const leftClick = { button: 0 };
+  expect(await container.queryByText(formMessage)).toBeNull();
+  rtl.fireEvent.click(getByText(/submit/i), leftClick);
+
+  expect(await modal.getByText(formMessage)).toBeInTheDocument();
+  await navigate('/');
+});
