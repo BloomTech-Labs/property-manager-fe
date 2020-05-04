5b399a68cc3850da9fbfc6669d9a4b18c6e7812c
nick.durbin <nick.durbin@gmail.com>
2020-05-04 16:48:03 -0400

    Testing the navigation of the to go to signup.

--- src/views/landing/__test__/LandingPage.spec.js
+++ src/views/landing/__test__/LandingPage.spec.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import LandingPage from '../LandingPage';
+import renderWithRouter from '../../../helpers/renderWithRouter';
 
 afterEach(rtl.cleanup);
 
-test('renders the entire LandingPage Component', async () => {
+test('Renders Landing Page and checks button navigation', async () => {
+  const {
+    container,
+    getByText,
+    history: { navigate }
+  } = renderWithRouter(<LandingPage />);
+  const landingContainer = container;
+  expect(await landingContainer.innerHTML).toMatch(/freehold/i);
+
+  const leftClick = { button: 0 };
+  rtl.fireEvent.click(getByText(/get started/i), leftClick);
+  await navigate('/signup');
+  expect(await container.innerHTML).toMatch(/sign up/i);
+});
+
+test('Makes sure the word Freehold exists on LP', async () => {
   const wrapper = rtl.render(<LandingPage />);
   expect(await wrapper.queryAllByText(/freehold/i));
 });
