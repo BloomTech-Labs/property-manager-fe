5797cfb1b1a833489ac90278300fd2a96678b0c3
nick.durbin <nick.durbin@gmail.com>
2020-05-05 20:35:59 -0400

    Added some basic tests with a snapshot for the Header component.

--- src/views/landing/Header/__test__/Header.spec.js
+++ src/views/landing/Header/__test__/Header.spec.js
@@ -5,7 +5,19 @@ import Header from '../Header';
 
 afterEach(rtl.cleanup);
 
-test('Makes sure the word Freehold exists on LP', async () => {
+test('Makes sure the word property exists in Header', async () => {
   const wrapper = rtl.render(<Header />);
   expect(await wrapper.queryAllByText(/property/i));
+});
+
+test('Looks for Get Started Button and tests functionality', async () => {
+  const wrapper = rtl.render(<Header />);
+  const leftClick = { button: 0 };
+
+  rtl.fireEvent.click(await wrapper.getByText('Get Started'), leftClick);
+});
+
+test('snapshot', () => {
+  const { container } = rtl.render(<Header />);
+  expect(container.firstChild).toMatchSnapshot();
 });
