182ef099c29ada9e479b2e144eb20eee88f7fce8
nick.durbin <nick.durbin@gmail.com>
2020-05-05 20:35:37 -0400

    Added some basic tests with a snapshot for the Footer component.

--- src/views/landing/Footer/__test__/Footer.spec.js
+++ src/views/landing/Footer/__test__/Footer.spec.js
@@ -5,7 +5,19 @@ import Footer from '../Footer';
 
 afterEach(rtl.cleanup);
 
-test('Makes sure the word Freehold exists on LP', async () => {
+test('Makes sure Footer renders with Copyright Freehold exists on LP', async () => {
   const wrapper = rtl.render(<Footer />);
   expect(await wrapper.queryAllByText(/copyright freehold/i));
+});
+
+test('Looks for Contact Us link and tests functionality', async () => {
+  const wrapper = rtl.render(<Footer />);
+  const leftClick = { button: 0 };
+
+  rtl.fireEvent.click(await wrapper.getByText('Contact Us'), leftClick);
+});
+
+test('snapshot', () => {
+  const { container } = rtl.render(<Footer />);
+  expect(container.firstChild).toMatchSnapshot();
 });
