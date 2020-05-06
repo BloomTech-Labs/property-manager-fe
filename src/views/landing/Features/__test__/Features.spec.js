8c8ace86ad8507502fc802c69e3242b0011841b0
nick.durbin <nick.durbin@gmail.com>
2020-05-05 20:35:11 -0400

    Added some basic tests with a snapshot for the Features component.

--- src/views/landing/Features/__test__/Features.spec.js
+++ src/views/landing/Features/__test__/Features.spec.js
@@ -9,3 +9,8 @@ test('Makes sure the word Freehold exists on LP', async () => {
   const wrapper = rtl.render(<Features />);
   expect(await wrapper.queryAllByText(/professional dashboard/i));
 });
+
+test('snapshot', () => {
+  const { container } = rtl.render(<Features />);
+  expect(container.firstChild).toMatchSnapshot();
+});
