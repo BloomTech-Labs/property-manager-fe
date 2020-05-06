76661485c4c6997c0ef05e51084cb7c14780bca8
nick.durbin <nick.durbin@gmail.com>
2020-05-05 20:34:29 -0400

    Added some basic tests with a snapshot for the FeatureButtons component.

--- src/views/landing/Features/__test__/FeatureButtons.spec.js
+++ src/views/landing/Features/__test__/FeatureButtons.spec.js
@@ -5,7 +5,12 @@ import FeatureButton from '../FeatureButtons';
 
 afterEach(rtl.cleanup);
 
-test('Makes sure the word Freehold exists on LP', async () => {
+test('Makes sure the word buy exists on buttons', async () => {
   const wrapper = rtl.render(<FeatureButton />);
   expect(await wrapper.queryAllByText(/buy/i));
+});
+
+test('snapshot', () => {
+  const { container } = rtl.render(<FeatureButton />);
+  expect(container.firstChild).toMatchSnapshot();
 });
